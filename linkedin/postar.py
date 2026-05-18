044d61598c142d2635a1f663b8127c5819523a85
Oseias Nepomuceno <oseiasnepom@gmail.com>
2026-05-18 08:20:00 -0300

    Fix formatting in postar.py

--- linkedin/postar.py
+++ linkedin/postar.py
@@ -102,7 +102,7 @@ def publicar_linkedin(user_id, conteudo):
             "Content-Type": "application/json"
         }
         
-        }
+    
 
         # =========================
         # POSTAGEM
